f3f2ba05e7699ab8cc954d1d5c9a217964c66cfe
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-01-17 16:57:09 +0530

    Your commit message

---
+++ src/@core/services/ResponseService.js
@@ -0,0 +1,10 @@
+export function sendResponse(res, status, data, message, statusCode = 200, apiVersion = null) {
+    let obj = {
+        status,
+        data,
+        message,
+        apiVersion: apiVersion || 'No Version',
+    };
+
+    return res.status(statusCode).json(obj);
+}
