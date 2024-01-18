a2b885f3ad4484f027e40a0a3e84bc8653d7bb91
FahadKhan99 <fahad.khan44604@gmail.com>
2024-01-18 23:44:35 +0530

    "Added Inv / org routes"

--- src/app/modules/admin/adminRoutes.js
+++ src/app/modules/admin/adminRoutes.js
@@ -8,4 +8,14 @@ router.route("/users").post(adminController.saveUser);
 router.route("/users/:id").get(adminController.getSingleUser);
 router.route("/users/:id").delete(adminController.deleteUser);
 
+router.route("/organisations").get(adminController.getAllOrganisations);
+router.route("/organisations").post(adminController.saveOrganisation);
+router.route("/organisations/:id").get(adminController.getSingleOrganisation);
+router.route("/organisations/:id").delete(adminController.deleteOrganisation);
+
+router.route("/inventory").get(adminController.getAllInventories);
+router.route("/inventory").post(adminController.saveInventory);
+router.route("/inventory/:id").get(adminController.getSingleInventory);
+router.route("/inventory/:id").delete(adminController.deleteInventory);
+
 export default router;
