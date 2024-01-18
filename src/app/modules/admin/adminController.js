790edaf1528cb24d6b9c56e104c4c5406d6a0f02
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-01-18 13:46:49 +0530

    updated admin module and db user

--- src/app/modules/admin/adminController.js
+++ src/app/modules/admin/adminController.js
@@ -1,5 +1,9 @@
-import { sendResponse, logger } from "@core/services/ResponseService";
+import statusType from "@/@core/enum/statusTypes";
+import logger from "@/@core/services/LoggingService";
+import { sendResponse } from "@core/services/ResponseService";
+import prisma from "@/@core/helpers/prisma";
 
+// User Functions
 export async function getAllUsers(req, res) {
   try {
     return sendResponse(res, true, null, "Api Not Ready Yet");
@@ -26,3 +30,16 @@ export async function getSingleUser(req, res) {
     return sendResponse(res, false, null, "Error ", statusType.DB_ERROR);
   }
 }
+
+export async function deleteUser(req, res) {
+  try {
+    return sendResponse(res, true, null, "Api Not Ready Yet");
+  } catch (error) {
+    logger.consoleErrorLog(req.originalUrl, "Error in functionName", error);
+    return sendResponse(res, false, null, "Error ", statusType.DB_ERROR);
+  }
+}
+
+// Organisation Functions
+
+// Inventory Functions
--- src/app/modules/admin/adminRoutes.js
+++ src/app/modules/admin/adminRoutes.js
@@ -6,5 +6,6 @@ import * as adminController from "./adminController";
 router.route("/users").get(adminController.getAllUsers);
 router.route("/users").post(adminController.saveUser);
 router.route("/users/:id").get(adminController.getSingleUser);
+router.route("/users/:id").delete(adminController.deleteUser);
 
 export default router;
