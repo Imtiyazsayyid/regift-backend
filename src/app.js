06bed0b86bd54a2448d3409e1456695d8343ae5e
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-02-02 22:14:14 +0530

    Cors test

--- src/app.js
+++ src/app.js
@@ -12,21 +12,6 @@ app.set("port", process.env.PORT || 8003);
 app.use(express.static("public"));
 app.use(bodyParser.json({ limit: "500mb" }));
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
 app.use(cors());
 app.use(cookieParser());
 app.use(
@@ -37,9 +22,6 @@ app.use(
   })
 );
 
-app.use(allowCors());
-
 app.use(router);
-// Test
 
 export default app;
